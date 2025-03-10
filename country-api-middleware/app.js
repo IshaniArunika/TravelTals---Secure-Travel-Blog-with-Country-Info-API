5e8fc8aa85d3c98bdaec376786d7d03890c8d4e5
Ishani Arunika <ishani.20210285@iit.ac.lk>
2025-03-10 13:17:19 +0530

    main folder structure

--- country-api-middleware/app.js
+++ country-api-middleware/app.js
@@ -1,9 +1,19 @@
 const express =  require('express');
 const PORT = process.env.PORT || 4000;
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+ 
 
 const app = express();
+dotenv.config();
+ 
+
 
 app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
-    
+    console.log(`Server running on port ${PORT}`);   
 })
+
+
+ 
+
+
