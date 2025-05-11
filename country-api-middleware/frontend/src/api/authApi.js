c2610507794306e60ccd45912bfa747954d07563
Ishani Arunika <123758631+IshaniArunika@users.noreply.github.com>
2025-05-11 19:34:21 +0530

    add missing cw1 frontend

---
+++ country-api-middleware/frontend/src/api/authApi.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:4000/auth';
+
+export const registerUser = async ({ username, email, password }) => {
+  const response = await axios.post(`${BASE_URL}/register`, {
+    username,
+    email,
+    password,
+  });
+  return response.data;
+};
+
+export const loginUser = async ({ email, password }) => {
+  const response = await axios.post(`${BASE_URL}/login`, {
+    email,
+    password,
+  });
+  return response.data;
+};
